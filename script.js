f985cf6b2bfdcfb4dc9c2661a757d1e4259033d9
Mohit Tiwari <mohittiwari17091709@gmail.com>
2025-07-02 22:29:06 +0530

    script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const API_URL = "";
 const { jsPDF } = window.jspdf;
 let metricsChart;
 
-//f
+//fo
 const themeToggle = document.getElementById('theme-toggle');
 if (themeToggle) {
     themeToggle.addEventListener('click', () => {
