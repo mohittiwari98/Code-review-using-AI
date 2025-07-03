925b983a3c50f1dfe1e8dfaa16493f6d0e7d1d8c
Mohit Tiwari <mohittiwari17091709@gmail.com>
2025-07-03 12:02:56 +0530

    script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ if (themeToggle) {
         document.body.classList.toggle('dark');
         localStorage.setItem('theme', document.body.classList.contains('dark') ? 'dark' : 'light');
     });
-   // store 
+   // store i
     if (localStorage.getItem('theme') === 'dark') {
         document.body.classList.add('dark');
     }
