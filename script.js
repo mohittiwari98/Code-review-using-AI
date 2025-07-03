ff4d25ece17db812ce82f0a799c76308175edfc8
Mohit Tiwari <mohittiwari17091709@gmail.com>
2025-07-03 17:13:52 +0530

    script.js

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ if (themeToggle) {
 }
 
 
-//generate ren
+//generate rend
 function renderChart(metrics) {
     if (!document.getElementById('metrics-chart')) {
         console.error('Metrics chart canvas not found');
